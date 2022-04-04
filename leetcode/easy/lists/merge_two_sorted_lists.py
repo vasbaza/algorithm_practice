b4d3d170dde374728fb96a757da9038d31f30dff
Maria Vasilchenko-Bazarova <maria.vasbaza@gmail.com>
2022-04-04 20:14:58 +0300

    merge two sorted lists

--- leetcode/easy/lists/merge_two_sorted_lists.py
+++ leetcode/easy/lists/merge_two_sorted_lists.py
@@ -1,2 +1,50 @@
-# def mergeTwoLists(self, l1, l2):
-#     l1 = [1, 2, 4], l2 = [1, 3, 4]
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def mergeTwoLists(list1, list2):
+    if not list1 or not list2:
+        return list1 or list2
+
+    if list1.val <= list2.val:
+        root = ListNode(list1.val, None)
+
+        root.next = mergeTwoLists(list1.next, list2)
+    else:
+        root = ListNode(list2.val, None)
+        root.next = mergeTwoLists(list1, list2.next)
+    return root
+
+
+# node1_3 = ListNode(val=4)
+# node1_2 = ListNode(val=2, next_val=node1_3)
+# node1_1 = ListNode(val=1, next_val=node1_2)
+#
+# node2_3 = ListNode(val=4)
+# node2_2 = ListNode(val=3, next_val=node2_3)
+# node2_1 = ListNode(val=1, next_val=node2_2)
+
+# node1_3 = ListNode(val=4)
+# node1_2 = ListNode(val=2, next_val=node1_3)
+# node1_1 = ListNode(val=1, next_val=node1_2)
+#
+# node2_1 = ListNode(val=5)
+
+# node2_3 = ListNode(val=4)
+# node2_2 = ListNode(val=2, next_val=node2_3)
+# node2_1 = ListNode(val=1, next_val=node2_2)
+#
+# node1_1 = ListNode(val=5)
+
+node2_2 = ListNode(val=3)
+node2_1 = ListNode(val=-9, next=node2_2)
+
+node1_1 = ListNode(val=7)
+node1_1 = ListNode(val=5, next=node1_1)
+
+mergeTwoLists(node1_1, node2_1)
